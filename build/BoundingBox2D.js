8fdc77feb5edd36c46abd4d025d8455d7f5bedd9
michaelnicol <michaelnicol71@gmail.com>
2023-07-06 12:13:29 -0400

    Added Dimension and Area Methods

--- build/BoundingBox2D.js
+++ build/BoundingBox2D.js
@@ -17,15 +17,28 @@ export class BoundingBox2D {
      */
     UR;
     center;
+    xRange;
+    yRange;
     constructor(UL, UR, BL, BR) {
         this.UL = UL.clone();
         this.UR = UR.clone();
         this.BL = BL.clone();
         this.BR = BR.clone();
         this.center = Utilities.pointCenter([this.BL, this.BR, this.UL, this.UR]);
+        this.xRange = Math.abs(UR.arr[0] - UL.arr[0]);
+        this.yRange = Math.abs(UL.arr[1] - BL.arr[1]);
     }
     toPrint() {
         return `[${this.UL.toPrint()}, ${this.UR.toPrint()}, ${this.BL.toPrint()}, ${this.BR.toPrint()}]`;
+    }
+    getCoordinateList() {
+        return [this.UL.clone(), this.UR.clone(), this.BL.clone(), this.BR.clone()];
+    }
+    canDimensionsFit(box) {
+        return (this.UL.arr[0] + box.xRange <= this.UR.arr[0]) && (this.UL.arr[1] + box.yRange <= this.BL.arr[1]);
+    }
+    area() {
+        return Utilities.pythagorean(this.UL, this.UR) * Utilities.pythagorean(this.BL, this.BR);
     }
     clone() {
         return new BoundingBox2D(this.UL, this.UR, this.BL, this.BR);
--- source/BoundingBox2D.ts
+++ source/BoundingBox2D.ts
@@ -30,13 +30,19 @@ export class BoundingBox2D implements ValidObject, cloneable<BoundingBox2D> {
       this.BR = BR.clone();
       this.center = Utilities.pointCenter([this.BL, this.BR, this.UL, this.UR])
       this.xRange = Math.abs(UR.arr[0] - UL.arr[0])
-      this.yRange = Math.abs(UL.arr[0] - BL.arr[0])
+      this.yRange = Math.abs(UL.arr[1] - BL.arr[1])
    }
    toPrint(): string {
-       return `[${this.UL.toPrint()}, ${this.UR.toPrint()}, ${this.BL.toPrint()}, ${this.BR.toPrint()}]`
+      return `[${this.UL.toPrint()}, ${this.UR.toPrint()}, ${this.BL.toPrint()}, ${this.BR.toPrint()}]`
    }
    getCoordinateList(): Point2D[] {
       return [this.UL.clone(), this.UR.clone(), this.BL.clone(), this.BR.clone()]
+   }
+   canDimensionsFit(box: BoundingBox2D): boolean {
+      return (this.UL.arr[0] + box.xRange <= this.UR.arr[0]) && (this.UL.arr[1] + box.yRange <= this.BL.arr[1])
+   }
+   area(): number {
+      return Utilities.pythagorean(this.UL, this.UR) * Utilities.pythagorean(this.BL, this.BR)
    }
    clone(): BoundingBox2D {
       return new BoundingBox2D(this.UL, this.UR, this.BL, this.BR)
